7e6ae7c08d2670043afebb48e4c7c504ce1fa283
Zachariah0318 <102947660+Zachariah0318@users.noreply.github.com>
2022-07-19 20:02:51 +0800

    Add files via upload

---
+++ homework_02/homework_02/Student.m
@@ -0,0 +1,15 @@
+//
+//  Student.m
+//  homework_02
+//
+//  Created by 王子俊 on 2022/7/19.
+//
+
+#import "Student.h"
+
+@implementation Student
+-(void)study:(double)time
+{
+    NSLog(@"%@ has learned %.2f hour(s).",name,time);
+}
+@end
